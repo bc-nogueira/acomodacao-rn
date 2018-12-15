add613d85fc95b98b6f9a9c346c8c84c73721a70
bc-nogueira <bcnogueira@id.uff.br>
2018-12-15 19:25:50 -0200

    Exibindo todas as informações da acomodação.

--- src/screens/Description.js
+++ src/screens/Description.js
@@ -32,13 +32,77 @@ export default class Description extends Component {
             .catch(error => console.log("Erro: " + error))
     }
 
+    tipoEmTexto() {
+        switch (this.state.acomodacao.tipo) {
+            case 0:
+                return 'Apartamento';
+            case 1:
+                return 'Casa';
+            case 2:
+                return 'Hotel';
+        }
+    }
+
     render() {
+        const { acomodacao } = this.state;
         return (
             <ScrollView>
                 {this.state && this.state.acomodacao &&
                     <View>
-                        <Text style={{padding:10, fontSize:20}}>{this.state.acomodacao.titulo}</Text>
-                        <Text style={{padding:10}}>{this.state.acomodacao.descricao}</Text>
+                        <Text style={{ marginTop: 10, fontWeight: 'bold', fontSize: 24, textAlign: 'center' }}>
+                            {acomodacao.titulo}
+                        </Text>
+
+                        <Text style={{ textAlign: 'center' }}>
+                            {this.tipoEmTexto()}
+                        </Text>
+
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 5 }}>
+                            <View style={{ flexDirection: 'row', marginLeft: 10 }}>
+                                <Text style={{ fontWeight: 'bold' }}>Capacidade: </Text>
+                                <Text>{acomodacao.capacidade}</Text>
+                            </View>
+                            <View style={{ flexDirection: 'row', marginRight: 10 }}>
+                                <Text style={{ fontWeight: 'bold' }}>Preço: </Text>
+                                <Text>{acomodacao.preco}</Text>
+                            </View>
+                        </View>
+
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 5 }}>
+                            <View style={{ flexDirection: 'row', marginLeft: 10 }}>
+                                <Text style={{ fontWeight: 'bold' }}>Logradouro: </Text>
+                                <Text>{acomodacao.logradouro}</Text>
+                            </View>
+                            <View style={{ flexDirection: 'row', marginRight: 10 }}>
+                                <Text style={{ fontWeight: 'bold' }}>Número: </Text>
+                                <Text>{acomodacao.numero}</Text>
+                            </View>
+                        </View>
+
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 5 }}>
+                            <View style={{ flexDirection: 'row', marginLeft: 10 }}>
+                                <Text style={{ fontWeight: 'bold' }}>Complemento: </Text>
+                                <Text>{acomodacao.complemento}</Text>
+                            </View>
+                            <View style={{ flexDirection: 'row', marginRight: 10 }}>
+                                <Text style={{ fontWeight: 'bold' }}>Bairro: </Text>
+                                <Text>{acomodacao.bairro}</Text>
+                            </View>
+                        </View>
+
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 5 }}>
+                            <View style={{ flexDirection: 'row', marginLeft: 10 }}>
+                                <Text style={{ fontWeight: 'bold' }}>Cidade: </Text>
+                                <Text>{acomodacao.cidade}</Text>
+                            </View>
+                            <View style={{ flexDirection: 'row', marginRight: 10 }}>
+                                <Text style={{ fontWeight: 'bold' }}>Estado: </Text>
+                                <Text>{acomodacao.estado}</Text>
+                            </View>
+                        </View>
+
+                        <Text style={{ marginLeft: 10, fontWeight: 'bold' }}>Descrição:</Text>
+                        <Text style={{ marginLeft: 10 }}>{acomodacao.descricao}</Text>
                     </View> 
                 }
                 {this.state.urls.map((url, index) => {
